6bac7b7016d5282892f1ed788d5d394d691e6753
ayman <aymanjaber2012@gmail.com>
2021-02-10 17:47:52 +0200

    fix review issues

--- index.rb
+++ index.rb
@@ -1,20 +1,20 @@
 def bubble_sort_by(string_array)
   repeat_process = false
   string_array[0..string_array.length - 2].each_with_index do |_item, index|
-    comparator_value = string_array[index] <=> string_array[index + 1]
-    next unless comparator_value.positive?
+    next unless yield(string_array[index], string_array[index + 1])
 
     value_holder = string_array[index]
     string_array[index] = string_array[index + 1]
     string_array[index + 1] = value_holder
     repeat_process = true
+
     # In case the value is 0 or -1, we can keep the array as is
   end
 
   if repeat_process
-    bubble_sort_by(string_array)
+    bubble_sort_by(string_array) { |firstString, secondString| firstString > secondString }
   else
-    string_array
+    string_array.join(',')
   end
 end
 
@@ -34,51 +34,11 @@ def bubble_sort(int_array)
   if repeat_process
     bubble_sort(int_array)
   else
-    int_array
+    int_array.join(',')
   end
-end
-
-def check_validity(string_array)
-  all_valid = string_array.is_a?(Array) && check_all_strings(string_array) && check_none_empty(string_array)
-
-  raise 'Invalid data entered, please enter an non-empty array of strings' unless all_valid
-
-  bubble_sort_by(string_array)
-end
-
-def check_validity_integer(int_array)
-  all_valid = int_array.is_a?(Array) && check_all_ints(int_array) && check_none_empty(int_array)
-
-  raise 'Invalid data entered, please enter an non-empty array of ints' unless all_valid
-
-  bubble_sort(int_array)
-end
-
-def check_none_empty(string_array)
-  none_empty = true
-  string_array.each do |string_word|
-    none_empty = false if string_word == ''
-  end
-  none_empty
-end
-
-def check_all_strings(string_array)
-  all_strings = true
-  string_array.each do |string_word|
-    all_strings = false unless string_word.is_a? String
-  end
-  all_strings
-end
-
-def check_all_ints(int_array)
-  all_ints = true
-  int_array.each do |int_number|
-    all_ints = false unless int_number.is_a? Integer
-  end
-  all_ints
 end
 
 # Enter the array of strings here
-puts check_validity(%w[cool perfect nice apple])
+puts bubble_sort_by(%w[cool perfect nice apple]) { |firstString, secondString| firstString > secondString }
 # Enter the array of integers here
-puts check_validity_integer([2, 3, 5, 3, 7, 0, 45])
+puts bubble_sort([2, 3, 5, 3, 7, 0, 45])
